60d8845db7f02823786dfc6abb761d0252d3677f
Paweł Pietras <pawel.pietras144@gmail.com>
2024-09-28 03:30:24 +0200

    couple of mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { driver } from './interfaceDriver';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Driver } from './interfaceDriver';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'projekt-4-zadanie-dyrektywy';
-  drivers: driver[] = [
-    {
-      firstName: 'Lewis',
-      lastName: 'Hamilton',
-      country: 'United Kingdom',
-      wins: 103,
-      team: 'Mercedes',
-    },
-    {
-      firstName: 'Max',
-      lastName: 'Verstappen',
-      country: 'Netherlands',
-      wins: 47,
-      team: 'Red Bull Racing',
-    },
-    {
-      firstName: 'Charles',
-      lastName: 'Leclerc',
-      country: 'Monaco',
-      wins: 5,
-      team: 'Ferrari',
-    },
-    {
-      firstName: 'Sebastian',
-      lastName: 'Vettel',
-      country: 'Germany',
-      wins: 53,
-      team: 'Aston Martin',
-    },
-    {
-      firstName: 'Fernando',
-      lastName: 'Alonso',
-      country: 'Spain',
-      wins: 32,
-      team: 'Alpine',
-    },
+  loading: boolean = true;
+  drivers: Driver[] = [];
+  columns: string[] = [
+    'driver_number',
+    'full_name',
+    'team_name',
+    'headshot_url',
+    'country_code',
   ];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchDrivers();
+  }
+
+  fetchDrivers(): void {
+    const apiUrl = 'https://api.openf1.org/v1/drivers?session_key=9158';
+    this.loading = true;
+    this.http.get<Driver[]>(apiUrl).subscribe(
+      (data) => {
+        this.drivers = data.map((driver) => {
+          return this.columns.reduce((acc, key) => {
+            acc[key] = driver[key];
+            return acc;
+          }, {} as any);
+        });
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Błąd podczas pobierania danych:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  objectKeys(obj: object): string[] {
+    return Object.keys(obj);
+  }
 }
--- src/app/interfaceDriver.ts
+++ src/app/interfaceDriver.ts
@@ -1,14 +1,15 @@
-export type Team =
-  | 'Mercedes'
-  | 'Red Bull Racing'
-  | 'Ferrari'
-  | 'Aston Martin'
-  | 'Alpine';
-
-export interface driver {
-  firstName: string;
-  lastName: string;
-  country: string;
-  wins: number;
-  team: Team;
+export interface Driver {
+  driver_number: number;
+  broadcast_name: string;
+  full_name: string;
+  name_acronym: string;
+  team_name: string;
+  team_colour: string;
+  first_name: string;
+  last_name: string;
+  headshot_url: string | null;
+  country_code: string;
+  session_key: number;
+  meeting_key: number;
+  [key: string]: string | number | null;
 }
